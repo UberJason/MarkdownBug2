fc4b8d9789e27210237faf634506670eb829cb72
Jason Ji <uberjason@gmail.com>
2021-06-13 19:44:06 -0400

    Initial Commit

---
+++ MarkdownBug2/MarkdownBug2App.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownBug2App.swift
+//  MarkdownBug2
+//
+//  Created by Jason Ji on 6/13/21.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownBug2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
